0a74a1adb5f59774e56550e7659478f5d8ff28e6
MiPiloto <walmir.junior@sou.unifeob.edu.br>
2022-06-24 13:29:03 -0300

    Initial

---
+++ src/components/HelloWorld/index.jsx
@@ -0,0 +1,9 @@
+export const HelloWorld = () => (
+  <div className="container">
+    <h1>Hello World!</h1>
+    <h2>Hi</h2>
+    <h3>Are you OK?</h3>
+    <h4>No.</h4>
+    <h5>Bye World!</h5>
+  </div>
+);
